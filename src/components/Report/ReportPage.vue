8a51ea313dd72415b03ab52ba980b2298bf552a3
aiman <maiman.ar@live.iium.edu.my>
2024-05-22 23:14:14 +0800

    updates

--- src/components/Report/ReportPage.vue
+++ src/components/Report/ReportPage.vue
@@ -1,27 +1,68 @@
-<script setup>
-
-import {
-  kButton as KButton, kList as KList,
-  kListInput as KListInput
-} from "konsta/vue";
-</script>
-
 <template>
-  <section>
-    <k-list>
-      <k-list-input label="SKU" type="text" placeholder="#SKU12345"/>
-      <k-list-input label="Date" type="date" placeholder="Your asset name"/>
-      <k-list-input label="Remark Issues" type="text" placeholder="Your asset name"/>
-      <k-list-input label="Remarks By" type="text" placeholder="Your asset name"/>
-    </k-list>
-    <section class="flex gap-2 mx-5 my-4">
-      <k-button>Cancel</k-button>
-      <k-button>Submit</k-button>
-    </section>
-
-  </section>
+    <k-page>
+        <k-navbar title="REPORT" />
+        <k-block-title>Report Asset View</k-block-title>
+        <div class ="block overflow-x-auto mt-8">
+            <k-table>
+                <k-table-head>
+                    <k-table-row header>
+                        <k-table-cell header> ID</k-table-cell>
+                        <k-table-cell header> SKU</k-table-cell>
+                        <k-table-cell header class="text-right"> DATE </k-table-cell>
+                        <k-table-cell header class="text-right"> ISSUES </k-table-cell>
+                        <k-table-cell header class="text-right"> REMARKS BY </k-table-cell>
+                    </k-table-row>
+                </k-table-head>
+                <k-table-body>
+                    <k-table-row v-for="report in reports">
+                        <k-table-cell>{{report.id}}</k-table-cell>
+                        <k-table-cell>{{report.sku}}</k-table-cell>
+                        <k-table-cell class="text-right">{{report.reportDate}}</k-table-cell>
+                        <k-table-cell class="text-right">{{report.reportDescription}}</k-table-cell>
+                        <k-table-cell class="text-right">{{report.reportBy}}</k-table-cell>
+                    </k-table-row>
+                    </k-table-body>
+            </k-table>
+        </div>
+    </k-page>
 </template>
 
-<style scoped>
+<script setup>
+import {onMounted, ref} from 'vue';
+import {
+  kBlockTitle,
+  kList,
+  kListInput, kApp as KApp, kButton as KButton,
+    kCard,
+    kTable,
+    kTableHead,
+    kTableBody,
+    kTableRow,
+    kTableCell,
+    kPage,
+    kNavbar
 
-</style>
+
+} from "konsta/vue";
+import {useRouter} from "vue-router";
+
+const reports = ref([]);
+const router = useRouter();
+function getReports(){
+  fetch('https://localhost:7043/Report/GetReports', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(response => {
+    return response.json();
+  }).then(data => {
+    reports.value = data;
+  });
+}
+
+onMounted(() => {
+  getReports();
+});
+
+</script>
