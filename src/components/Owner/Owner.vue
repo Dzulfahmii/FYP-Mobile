480dc9a6668c1bf651d26d0ef499295a3aeef6aa
aiman <maiman.ar@live.iium.edu.my>
2024-05-23 00:37:36 +0800

    updates

---
+++ src/components/Owner/Owner.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {onMounted, ref} from 'vue';
+import {
+  kBlockTitle,
+  kList,
+  kListItem,
+  kApp as KApp, kButton as KButton,
+  kPage,
+  kNavbar
+
+
+} from "konsta/vue";
+</script>
+
+<template>
+  <section>
+    <k-block-title>Information</k-block-title>
+    <k-list strong  inset>
+      <k-list-item class="font-bold border-b" title="Asset Name" >
+        <template #inner>
+        </template>
+      </k-list-item>
+    </k-list>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
